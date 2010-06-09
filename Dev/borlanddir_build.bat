50d339c4f359bca0d259f4a76f8ee044cdb06bcd
www-data <www-data@csgroup.eu>
2010-06-09 09:10:54 +0000

    Base

---
+++ Dev/borlanddir_build.bat
@@ -0,0 +1,17 @@
+cd bin
+mkdir bl
+cd bl
+mkdir debug
+mkdir release
+cd ..
+svn add bl
+cd ..
+cd lib
+mkdir bl
+cd bl
+mkdir debug
+mkdir release
+cd ..
+svn add bl
+
+
---
+++ Dev/vcdir_build.bat
@@ -0,0 +1,16 @@
+cd bin
+mkdir vc
+cd vc
+mkdir debug
+mkdir release
+cd ..
+svn add vc
+cd ..
+cd lib
+mkdir vc
+cd vc
+mkdir debug
+mkdir release
+cd ..
+svn add vc
+
