217539319a4c1f34aa0b645810043964f0efe755
skc <skc@csgroup.eu>
2017-08-02 13:02:57 +0000

    [SKC] retour à la revision 419 + merge branche NMM22 V1.8

---
+++ src/SwService/ServiceAlert/_SwResponse.cpp
@@ -0,0 +1,52 @@
+#include "_SwResponse.h"
+#include "ErrorEnd.h"
+
+
+
+
+StreamWork::SwCore::SwResponse::SwResponse(QString name, QString desc, alert::AlertManager * am, alert::ProcessId source) :Response(source), _desc(desc), _name(name), _am(am)
+{
+
+}
+
+StreamWork::SwCore::SwResponse::~SwResponse()
+{
+
+}
+
+
+void StreamWork::SwCore::SwResponse::setDescription(QString desc)
+{
+    _desc=desc;
+}
+
+void StreamWork::SwCore::SwResponse::setName(QString name)
+{
+    _name=name;
+}
+
+QString StreamWork::SwCore::SwResponse::getDescription()
+{
+    return _desc;
+}
+
+QString StreamWork::SwCore::SwResponse::getName()
+{
+    return _name;
+}
+
+void StreamWork::SwCore::SwResponse::execute()
+{
+    //qDebug() << "Executing SwRespond " << _name;
+
+    //// envoi fin d'erreur
+    //alert::ProcessId processId(QUuid::createUuid(), 0);
+    //alert::IErrorEnd *errorEnd = new alert::ErrorEnd(processId, getProcessId());
+    //// leveé de l'alert
+    //if (_am)
+    //{
+    //    _am->raiseErrorEnd(errorEnd);
+    //}
+
+	throw std::logic_error("The method or operation is not implemented.");
+}
