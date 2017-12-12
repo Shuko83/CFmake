f153968e8b36d90c025431f63163c5e02df54408
grt <grt@csgroup.eu>
2017-12-12 16:32:55 +0000

    (refs #13183) rev GAE Corrections des warnings C4505 et C4121 de SwServiceManager_Helper

--- Sources/SwCore/Main/Services/Management/SwServiceManager_Helper.h
+++ Sources/SwCore/Main/Services/Management/SwServiceManager_Helper.h
@@ -18,17 +18,38 @@ public:
 	SERVICE_TYPE * getService() const;
 
 	template <typename YOUR_CLASS>
-	void setService(QString serviceName, YOUR_CLASS* thisPointer, void (YOUR_CLASS::*callback)(bool available));	// ATTENTION: ne peut être appelée qu'une seule fois
+	void setService(QString serviceName, YOUR_CLASS* thisPointer, void (YOUR_CLASS::*callback)(bool available));
 	
-	void setService(QString serviceName);																			// ATTENTION: ne peut être appelée qu'une seule fois
+	void setService(QString serviceName);
 
 private:
 	QString _serviceName;
 	SERVICE_TYPE * _service;
 	std::function<void(bool)> _callback;
 
-	virtual void OnRegisterService(StreamWork::SwCore::ISwService * service);
-	virtual void OnUnregisterService(StreamWork::SwCore::ISwService * service);
+	// Inline pour eviter le warning C4505 remonté par visual incorrectement sur les methodes virtual dans des classes template
+	virtual void OnRegisterService(StreamWork::SwCore::ISwService * service) override
+	{
+		if (service->GetServiceName() == _serviceName)
+		{
+			if (_callback)
+				_callback(false);
+
+			_service = nullptr;
+		}
+	}
+
+	// Inline pour eviter le warning C4505 remonté par visual incorrectement sur les methodes virtual dans des classes template
+	virtual void OnUnregisterService(StreamWork::SwCore::ISwService * service) override
+	{
+		if (service->GetServiceName() == _serviceName)
+		{
+			_service = dynamic_cast<SERVICE_TYPE *>(service);
+
+			if (_callback)
+				_callback(true);
+		}
+	}
 };
 
 //---------------------------------------------------------------------------------
@@ -60,8 +81,11 @@ template <typename SERVICE_TYPE>
 template <typename YOUR_CLASS>
 void SwServiceManager_Helper<SERVICE_TYPE>::setService(QString serviceName, YOUR_CLASS* thisPointer, void (YOUR_CLASS::*callback)(bool))
 {
+	if (_service && _callback)
+		_callback(false);
+
 	_serviceName = serviceName;
-	_callback = ([=](bool available)->void { (thisPointer->*callback)(available); });
+	_callback = ([callback,thisPointer](bool available)->void { (thisPointer->*callback)(available); });
 	_service = dynamic_cast<SERVICE_TYPE *> (SW_APP->QueryService(serviceName));
 
 	if (_service)
@@ -74,30 +98,4 @@ void SwServiceManager_Helper<SERVICE_TYPE>::setService(QString serviceName)
 {
 	_serviceName = serviceName;
 	_service = dynamic_cast<SERVICE_TYPE *> (SW_APP->QueryService(serviceName));
-}
-
-//---------------------------------------------------------------------------------
-template <typename SERVICE_TYPE>
-void SwServiceManager_Helper<SERVICE_TYPE>::OnUnregisterService(StreamWork::SwCore::ISwService * service)
-{
-	if (service->GetServiceName() == _serviceName)
-	{
-		if (_callback)
-			_callback(false);
-
-		_service = nullptr;
-	}
-}
-
-//---------------------------------------------------------------------------------
-template <typename SERVICE_TYPE>
-void SwServiceManager_Helper<SERVICE_TYPE>::OnRegisterService(StreamWork::SwCore::ISwService * service)
-{
-	if (service->GetServiceName() == _serviceName)
-	{
-		_service = dynamic_cast<SERVICE_TYPE *>(service);
-
-		if (_callback)
-			_callback(true);
-	}
 }
