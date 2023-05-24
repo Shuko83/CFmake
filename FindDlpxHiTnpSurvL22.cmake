a19e3cbd916d33c3cb019facebf68ad1738d5527
Alexis Foerster <afoerste@sccoa.si.c-s.fr>
2023-05-24 11:00:00 +0200

    Update DLPX-PI dependencies (DLPX-HI).

--- FindDlpxHiTnpSurvL22.cmake
+++ FindDlpxHiTnpSurvL22.cmake
@@ -1,7 +1,7 @@
 find_dlpxhi_package_libraries(
   "DlpxHiTnpSurvL22"
   "TnpSurvL22"
-  "TnpSurvL22Objects;TnpSurvL22ReportReaders;TnpSurvL22ReportWriters"
+  "TnpSurvL22Objects;TnpSurvL22OrderReaders;TnpSurvL22OrderWriters;TnpSurvL22ReportReaders;TnpSurvL22ReportWriters"
   "Objects/LibEntry/TnpSurvL22ObjectsExport.h"
   "${TARGET_NAME}/debug"
   "${TARGET_NAME}/release")
