5ea2c40eb2da3f838bdcc8ffc507df937601351c
Alexis Foerster <afoerste@sccoa.si.c-s.fr>
2023-10-06 15:00:00 +0200

    Update DLPX-PI dependencies (LinkManagerFramework).

--- FindLnkMgrFwk.cmake
+++ FindLnkMgrFwk.cmake
@@ -11,3 +11,55 @@ find_package_libraries(
 if(NOT WIN32)
   add_library(LnkMgrFwk::LinkManagerFramework ALIAS LnkMgrFwk::LnkMgrFwk)
 endif(NOT WIN32)
+
+################################################################################
+# LinkManagerFramework runtime dependencies copy
+################################################################################
+
+function(copy_lnkmgrfwk_dependencies COMPONENT_NAME)
+
+  if(WIN32)
+
+    foreach(LIB_NAME abseil_dll cares libcrypto-1_1-x64 libssl-1_1-x64 re2)
+      add_custom_command(TARGET ${COMPONENT_NAME} POST_BUILD COMMAND cmake -E copy_if_different
+        ${LnkMgrFwk_DIR}/${TARGET_NAME}/$<IF:$<CONFIG:DEBUG>,debug,release>/${LIB_NAME}.dll ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+    endforeach(LIB_NAME abseil_dll cares libcrypto-1_1-x64 libssl-1_1-x64 re2)
+
+    set(LIB_NAME libprotobuf)
+    add_custom_command(TARGET ${COMPONENT_NAME} POST_BUILD COMMAND cmake -E copy_if_different
+      ${LnkMgrFwk_DIR}/${TARGET_NAME}/$<IF:$<CONFIG:DEBUG>,debug,release>/${LIB_NAME}$<IF:$<CONFIG:DEBUG>,d,>.dll ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
+    set(LIB_NAME_P zlib)
+    set(LIB_NAME_S 1)
+    add_custom_command(TARGET ${COMPONENT_NAME} POST_BUILD COMMAND cmake -E copy_if_different
+      ${LnkMgrFwk_DIR}/${TARGET_NAME}/$<IF:$<CONFIG:DEBUG>,debug,release>/${LIB_NAME_P}$<IF:$<CONFIG:DEBUG>,d,>${LIB_NAME_S}.dll ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
+  endif(WIN32)
+
+endfunction(copy_lnkmgrfwk_dependencies COMPONENT_NAME)
+
+################################################################################
+# LinkManagerFramework runtime dependencies installation
+################################################################################
+
+function(install_lnkmgrfwk_dependencies COMPONENT_NAME INSTALL_DIR)
+
+  if(WIN32)
+
+    foreach(LIB_NAME abseil_dll cares libcrypto-1_1-x64 libssl-1_1-x64 re2)
+      install(FILES ${LnkMgrFwk_DIR}/${TARGET_NAME}/$<IF:$<CONFIG:DEBUG>,debug,release>/${LIB_NAME}.dll
+        DESTINATION ${INSTALL_DIR} COMPONENT ${COMPONENT_NAME})
+    endforeach(LIB_NAME abseil_dll cares libcrypto-1_1-x64 libssl-1_1-x64 re2)
+
+    set(LIB_NAME libprotobuf)
+    install(FILES ${LnkMgrFwk_DIR}/${TARGET_NAME}/$<IF:$<CONFIG:DEBUG>,debug,release>/${LIB_NAME}$<IF:$<CONFIG:DEBUG>,d,>.dll
+      DESTINATION ${INSTALL_DIR} COMPONENT ${COMPONENT_NAME})
+
+    set(LIB_NAME_P zlib)
+    set(LIB_NAME_S 1)
+    install(FILES ${LnkMgrFwk_DIR}/${TARGET_NAME}/$<IF:$<CONFIG:DEBUG>,debug,release>/${LIB_NAME_P}$<IF:$<CONFIG:DEBUG>,d,>${LIB_NAME_S}.dll
+      DESTINATION ${INSTALL_DIR} COMPONENT ${COMPONENT_NAME})
+
+  endif(WIN32)
+
+endfunction(install_lnkmgrfwk_dependencies COMPONENT_NAME INSTALL_DIR)
