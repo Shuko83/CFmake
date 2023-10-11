7930176545469eb9ef903c0546b605cc345ac846
Alexis Foerster <afoerste@sccoa.si.c-s.fr>
2023-10-11 11:00:00 +0200

    Update DLPX-PI dependencies (CODX).

--- FindCodxAsterix.cmake
+++ FindCodxAsterix.cmake
@@ -1,6 +1,6 @@
-set_os_variable(CodxAsterixED${ASTERIX_EDITION}Pattern "CodxAsterixED${ASTERIX_EDITION}" "CodxAsterix")
-set_os_variable(CodxAsterixED${ASTERIX_EDITION}Elements "CodxAsterixED${ASTERIX_EDITION}Elements;CodxAsterixED${ASTERIX_EDITION}Elements1;CodxAsterixED${ASTERIX_EDITION}Elements2;CodxAsterixED${ASTERIX_EDITION}Elements3" "CodxAsterixElements")
-set_os_variable(CodxAsterixED${ASTERIX_EDITION}Messages "CodxAsterixED${ASTERIX_EDITION}Messages" "CodxAsterixMessages")
+set_os_variable(CodxAsterixED${ASTERIX_EDITION}Pattern "CodxAsterixED${ASTERIX_EDITION}" "CodxASTERIX")
+set_os_variable(CodxAsterixED${ASTERIX_EDITION}Elements "CodxAsterixED${ASTERIX_EDITION}Elements;CodxAsterixED${ASTERIX_EDITION}Elements1;CodxAsterixED${ASTERIX_EDITION}Elements2;CodxAsterixED${ASTERIX_EDITION}Elements3" "CodxASTERIXElements")
+set_os_variable(CodxAsterixED${ASTERIX_EDITION}Messages "CodxAsterixED${ASTERIX_EDITION}Messages" "CodxASTERIXMessages")
 
 find_codx_package_libraries(
   "CodxAsterix"
@@ -11,6 +11,8 @@ find_codx_package_libraries(
   "${DEP_TARGET_NAME}/release")
 
 if(NOT WIN32)
+  add_library(CodxAsterix::CodxASTERIXElements ALIAS CodxAsterix::Elements)
+  add_library(CodxAsterix::CodxASTERIXMessages ALIAS CodxAsterix::Messages)
   add_library(CodxAsterix::Elements1 ALIAS CodxAsterix::Elements)
   add_library(CodxAsterix::Elements2 ALIAS CodxAsterix::Elements)
   add_library(CodxAsterix::Elements3 ALIAS CodxAsterix::Elements)
