689d17ebba06d295c01e1fcf96ea15a7a397e1f5
giacomo.rombaut <giacomo.rombaut@csgroup.eu>
2024-10-11 14:31:05 +0200

    cstoolkit_fetch_artifactory: Legacy Config file now supports more libraries cstoolkit_fetch_artifactory: Now prints status message when downloading CPackRules: CPACK_INCLUDE_TOPLEVEL_DIRECTORY OFF and CPACK_VERBATIM_VARIABLES ON Fix issue with gcc version regex

--- utility/CPackRules.cmake
+++ utility/CPackRules.cmake
@@ -3,4 +3,7 @@ set(CPACK_PACKAGE_VENDOR "CSGroup" CACHE STRING "The name of the package vendor.
 set(CPACK_PACKAGE_DIRECTORY "${CMAKE_BINARY_DIR}/../install" CACHE STRING "The directory in which CPack is doing its packaging.")
 set(CPACK_PACKAGE_VERSION "${CMAKE_PROJECT_VERSION}" CACHE STRING "Package full version")
 
+set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY OFF)
+set(CPACK_VERBATIM_VARIABLES ON)
+
 include(CPack)
