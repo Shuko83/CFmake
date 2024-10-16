e387f7b6f18ff49dd023ac7850fbd3625513773c
giacomo.rombaut <giacomo.rombaut@csgroup.eu>
2024-10-16 16:58:29 +0200

    CPACK : Fix default package directory CPACK : Fix default package filename

--- utility/CPackRules.cmake
+++ utility/CPackRules.cmake
@@ -1,9 +1,10 @@
 set(CPACK_GENERATOR "7Z" CACHE STRING "List of CPack generators to use.")
 set(CPACK_PACKAGE_VENDOR "CSGroup" CACHE STRING "The name of the package vendor.")
-set(CPACK_PACKAGE_DIRECTORY "${CMAKE_BINARY_DIR}/../install" CACHE STRING "The directory in which CPack is doing its packaging.")
-set(CPACK_PACKAGE_VERSION "${CMAKE_PROJECT_VERSION}" CACHE STRING "Package full version")
+set(CPACK_PACKAGE_DIRECTORY "${CMAKE_BINARY_DIR}/../package" CACHE STRING "The directory in which CPack is doing its packaging.")
+set(CPACK_PACKAGE_VERSION "${CMAKE_PROJECT_VERSION}" CACHE STRING "Package full version.")
+set(CPACK_PACKAGE_FILE_NAME "${CMAKE_PROJECT_NAME}_${CMAKE_PROJECT_VERSION}_${CSTOOLKIT_BUILD_MKSPECS}" CACHE STRING "The name of the package file to generate.")
 
-set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY OFF)
-set(CPACK_VERBATIM_VARIABLES ON)
+set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY OFF CACHE BOOL "Boolean toggle to include/exclude top level directory.")
+set(CPACK_VERBATIM_VARIABLES ON CACHE BOOL "If set to TRUE, values of variables prefixed with CPACK_ will be escaped")
 
 include(CPack)
