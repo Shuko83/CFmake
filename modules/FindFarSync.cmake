2359364cca2c7136b12afc36503af101b90f0062
Anthony Julien <ajulien@sccoa.si.c-s.fr>
2025-01-31 08:30:31 +0000

    Find Modules: Added CrashRPT, FarSync, Pcap and WinPcap

---
+++ modules/FindFarSync.cmake
@@ -0,0 +1,19 @@
+cmake_minimum_required(VERSION 3.18)
+
+set(DELIVERY_DIR "${CMAKE_BINARY_DIR}/externals/${CMAKE_FIND_PACKAGE_NAME}")
+
+message("--->  Find ${CMAKE_FIND_PACKAGE_NAME} in ${DELIVERY_DIR}")
+
+
+if (NOT TARGET ${CMAKE_FIND_PACKAGE_NAME})
+  message("         IMPORT : ${CMAKE_FIND_PACKAGE_NAME}, ${DELIVERY_DIR}")
+  add_library(${CMAKE_FIND_PACKAGE_NAME} INTERFACE)
+  set_target_properties(${CMAKE_FIND_PACKAGE_NAME} PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${DELIVERY_DIR})
+else()
+  message("         target ${CMAKE_FIND_PACKAGE_NAME} already imported")
+endif()
+
+# handle the QUIETLY and REQUIRED arguments and set xxx_FOUND to TRUE if
+# all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(${CMAKE_FIND_PACKAGE_NAME} DEFAULT_MSG)
---
+++ modules/FindPcap.cmake
@@ -0,0 +1,34 @@
+cmake_minimum_required(VERSION 3.18)
+
+set(DELIVERY_DIR "${CMAKE_BINARY_DIR}/externals/${CMAKE_FIND_PACKAGE_NAME}")
+
+message("--->  Find ${CMAKE_FIND_PACKAGE_NAME} in ${DELIVERY_DIR}")
+
+file(GLOB_RECURSE LIBS_LIST ${DELIVERY_DIR}/*.a)
+
+
+foreach(lib ${LIBS_LIST})
+    get_filename_component(fileName ${lib} NAME_WE)
+
+    #remove debug extension if exist
+    string(REGEX REPLACE "(.*)d" "\\1" fileName ${fileName})
+
+
+    STRING(REPLACE ".lib" ".dll" dll ${lib})
+
+    if (NOT TARGET ${fileName})
+        message("         IMPORT : ${fileName}")
+        add_library(${fileName} STATIC IMPORTED)
+        set_target_properties(${fileName} PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${DELIVERY_DIR}/include)
+
+        set_target_properties(${fileName} PROPERTIES APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+        set_target_properties(${fileName} PROPERTIES IMPORTED_IMPLIB_DEBUG "${lib}")
+        set_target_properties(${fileName} PROPERTIES IMPORTED_LOCATION_DEBUG "${dll}")
+    else()
+        message("         target ${fileName} already imported")
+    endif()
+endforeach()
+
+set(ALIAS_LIST "")
+set(LIBS_LIST_STRING "")
+set(DLLS_LIST_STRING "")
