0a43c0fa71b809f5a395c11f63878ab1b7c6f45d
Anthony Julien <ajulien@sccoa.si.c-s.fr>
2025-05-12 09:14:18 +0000

    Add FindQwt

---
+++ modules/FindQwt.cmake
@@ -0,0 +1,48 @@
+cmake_minimum_required(VERSION 3.18)
+
+set(DELIVERY_DIR "${CMAKE_BINARY_DIR}/externals/${CMAKE_FIND_PACKAGE_NAME}")
+
+message("--->  Find ${CMAKE_FIND_PACKAGE_NAME} in ${DELIVERY_DIR}")
+
+file(GLOB_RECURSE LIBS_LIST ${DELIVERY_DIR}/*.lib)
+
+
+foreach(lib ${LIBS_LIST})
+    get_filename_component(fileName ${lib} NAME_WE)
+
+    #remove debug extension if exist
+    string(REGEX REPLACE "(.+)d$" "\\1" fileName ${fileName})
+    set(dirName ${fileName})
+
+    STRING(REPLACE ".lib" ".dll" dll ${lib})
+    MESSAGE("Lib : ${lib}")
+
+    if (NOT TARGET ${fileName})
+      message("         IMPORT : ${fileName}")
+      add_library(${fileName} SHARED IMPORTED)
+      set_target_properties(${fileName} PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${DELIVERY_DIR}/include/)
+    endif()
+
+    if(lib MATCHES "debug")
+      set_target_properties(${fileName} PROPERTIES APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+      set_target_properties(${fileName} PROPERTIES IMPORTED_IMPLIB_DEBUG "${lib}")
+      set_target_properties(${fileName} PROPERTIES IMPORTED_LOCATION_DEBUG "${dll}")
+    else()
+      set_target_properties(${fileName} PROPERTIES APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+      set_target_properties(${fileName} PROPERTIES IMPORTED_IMPLIB_RELEASE "${lib}")
+      set_target_properties(${fileName} PROPERTIES IMPORTED_LOCATION_RELEASE "${dll}")
+    endif()
+endforeach()
+
+set(ALIAS_LIST "")
+set(LIBS_LIST_STRING "")
+set(DLLS_LIST_STRING "")
+
+set(Qwt_RUNTIME_DLLS
+    "${Qwt_ROOT}/win-msvc2015-${CSTOOLKIT_HOST_ARCH}/qwt.dll"
+)
+
+# handle the QUIETLY and REQUIRED arguments and set xxx_FOUND to TRUE if
+# all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(${CMAKE_FIND_PACKAGE_NAME} DEFAULT_MSG Qwt_RUNTIME_DLLS)
