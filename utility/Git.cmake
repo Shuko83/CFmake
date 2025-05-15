2bd91c219dfb2092330279804b5ed5217bf0a5f8
giacomo.rombaut <giacomo.rombaut@csgroup.eu>
2025-05-15 14:49:36 +0200

    Add source_dir to gitinfo generation

--- utility/Git.cmake
+++ utility/Git.cmake
@@ -1,34 +1,38 @@
 find_package(Git QUIET)
 
 if(GIT_EXECUTABLE)
-    execute_process(COMMAND ${GIT_EXECUTABLE} describe --all --dirty --broken --long --always
-                    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    execute_process(COMMAND "${GIT_EXECUTABLE}" describe --all --dirty --broken --long --always
+                    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
                     OUTPUT_VARIABLE CSTOOLKIT_GIT_COMMIT
                     ERROR_QUIET)
     
-    execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
-                    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    execute_process(COMMAND "${GIT_EXECUTABLE}" rev-parse HEAD
+                    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
                     OUTPUT_VARIABLE CSTOOLKIT_GIT_SHA
                     ERROR_QUIET)
 
-    execute_process(COMMAND ${GIT_EXECUTABLE} describe --exact-match --tags
-                    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    execute_process(COMMAND "${GIT_EXECUTABLE}" describe --exact-match --tags
+                    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
                     OUTPUT_VARIABLE CSTOOLKIT_GIT_TAG
                     ERROR_QUIET)
 
-    execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
-                    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    execute_process(COMMAND "${GIT_EXECUTABLE}" rev-parse --abbrev-ref HEAD
+                    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
                     OUTPUT_VARIABLE CSTOOLKIT_GIT_BRANCH
                     ERROR_QUIET)
 
     execute_process(COMMAND ${GIT_EXECUTABLE} log -n 1 --pretty=%cD
-                    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+                    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
                     OUTPUT_VARIABLE CSTOOLKIT_GIT_DATE
                     ERROR_QUIET)
 
-    execute_process(COMMAND ${GIT_EXECUTABLE} remote get-url origin
-                    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    execute_process(COMMAND "${GIT_EXECUTABLE}" remote get-url origin
+                    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
                     OUTPUT_VARIABLE CSTOOLKIT_GIT_URL
+                    ERROR_QUIET)
+    execute_process(COMMAND "${GIT_EXECUTABLE}" rev-parse --show-toplevel
+                    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+                    OUTPUT_VARIABLE CSTOOLKIT_GIT_RELATIVE_TOP_LEVEL
                     ERROR_QUIET)
 
     string(STRIP "${CSTOOLKIT_GIT_COMMIT}" CSTOOLKIT_GIT_COMMIT)
@@ -37,8 +41,13 @@ if(GIT_EXECUTABLE)
     string(STRIP "${CSTOOLKIT_GIT_BRANCH}" CSTOOLKIT_GIT_BRANCH)
     string(STRIP "${CSTOOLKIT_GIT_DATE}" CSTOOLKIT_GIT_DATE)
     string(STRIP "${CSTOOLKIT_GIT_URL}" CSTOOLKIT_GIT_URL)
+    string(STRIP "${CSTOOLKIT_GIT_RELATIVE_TOP_LEVEL}" CSTOOLKIT_GIT_RELATIVE_TOP_LEVEL)
 
     if(CSTOOLKIT_GIT_BRANCH STREQUAL "HEAD")
         set(CSTOOLKIT_GIT_BRANCH "")
+    endif()
+
+    if(CSTOOLKIT_GIT_RELATIVE_TOP_LEVEL)
+        cmake_path(RELATIVE_PATH CMAKE_SOURCE_DIR BASE_DIRECTORY "${CSTOOLKIT_GIT_RELATIVE_TOP_LEVEL}" OUTPUT_VARIABLE CSTOOLKIT_GIT_RELATIVE_TOP_LEVEL)
     endif()
 endif()
